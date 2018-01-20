002b2ca075bff171b6fd292b9d1e50cf4d29eb4c
Mahmoud <mahmoud.khafagy@devinci.fr>
2018-01-20 11:31:11 +0100

    crud fini

--- resources/views/posts/article.blade.php
+++ resources/views/posts/article.blade.php
@@ -1,4 +1,3 @@
-
 @extends('layouts.app')
 
 @section('content')
@@ -6,42 +5,53 @@
     <div class="container">
         <div class="row">
 
-    <table class="table">
-        <thead class="thead-dark">
-        <tr>
+            <table class="table">
+                <thead class="thead-dark">
+                <tr>
 
-            <th scope="col">ID</th>
-            <th scope="col">Title</th>
-            <th scope="col">Tache</th>
-            <th scope="col">Actions</th>
+                    <th scope="col">ID</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Tache</th>
+                    <th scope="col">Actions</th>
 
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($posts as $post)
-            <tr>
-                <th scope="row">{{$post->id}}</th>
-                <td>{{$post->title}}</td>
-                <td>{{$post->body}}</td>
-                <td>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($posts as $post)
+                    <tr>
+                        <th scope="row">{{$post->id}}</th>
+                        <td>{{$post->title}}</td>
+                        <td>{{$post->body}}</td>
+                        <td>
+                            <a href="{{ route('edit', [$post->id]) }}" class="btn btn-primary btn-sm">
+                                <span class="glyphicon glyphicon-trash"></span> Edit
+                            </a>
 
-                    <a href="" onclick="event.preventDefault();
-                            document.getElementById('form-{!! $post->id !!}').submit();">
-                        Supprimer
-                    </a>
-                    <form id="form-{{$post->id}}" method="POST"  action="{{route('posts.destroy', [$post->id])}}">
-                        {{csrf_field()}}
-                        {{method_field('DELETE')}}
-                    </form>
+                            <span>|</span>
+                            <a href="{{ $post->id }}" class="btn btn-success btn-sm">
+                                <span class="glyphicon glyphicon-eye-open"></span> Show
+                            </a>
 
-                    <a href="{{ route('edit', [$post->id]) }}">Edit</a>
 
-                </td>
+                            <span>|</span>
 
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
+                            <a href="" class="btn btn-danger btn-sm"
+                               onclick="event.preventDefault();document.getElementById('form-{!! $post->id !!}').submit();">
+                                <span class="glyphicon glyphicon-trash"></span> Delete
+                            </a>
+                            <form id="form-{{$post->id}}" method="POST"
+                                  action="{{route('posts.destroy', [$post->id])}}">
+                                {{csrf_field()}}
+                                {{method_field('DELETE')}}
+                            </form>
+
+
+                        </td>
+
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
 
         </div>
     </div>
